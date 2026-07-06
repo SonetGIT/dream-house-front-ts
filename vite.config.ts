f723eb8e59ccc9ea667d355bd066e13772da8ed3
Sonet <sonetsaparbekova@gmail.com>
2026-07-06 17:47:06 +0600

    06.07

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
         },
     },
     server: {
-        // host: '0.0.0.0',
-        host: 'localhost',
+        host: '0.0.0.0',
+        // host: 'localhost',
         port: 5173,
     },
 });
